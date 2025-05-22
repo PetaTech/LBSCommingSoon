2dc4c4ff6b43898dd1af88c19d25dfdb1a534711
LBM <official@lovedbymen.com>
2025-05-22 02:41:27 +0200

    add fav ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'LovedByStyle - Coming Soon',
   description: 'Where Fashion Meets Mutual Support',
+  icons: {
+    icon: '/favicon.ico'
+  },
 }
 
 export default function RootLayout({
